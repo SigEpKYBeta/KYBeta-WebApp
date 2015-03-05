5757a900bebe61d0d1702f9c68d311a49e80288d
Burks, Sheldon <sburks@lexmark.com>
2015-03-05 09:57:37 -0500

    Added node, npm, and bower to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # # Install packages for development
 apt-get update -y
-apt-get install -y python3-pip
+apt-get install -y python3-pip curl
 
 # install and setup a virtualenv
 pip3 install virtualenv
@@ -10,6 +10,17 @@ cd /vagrant
 
 # install
 pip install Django
+
+# install node and npm and update
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install -y nodejs
+npm install npm -g
+
+# install bower
+npm install -g bower
+
+# install bower packages
+cd /vagrant
 
 #cleanup
 apt-get clean
