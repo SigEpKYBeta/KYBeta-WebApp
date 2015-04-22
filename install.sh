ac41cca098538c66c28b08aa8a2afa3e59d473b8
Burks, Sheldon <sburks@lexmark.com>
2015-04-22 16:33:58 -0400

    Lots of config changes

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 # Install packages for development
 echo "Updating Apt"
-apt-get update -y > /dev/null
+sudo apt-get update -y > /dev/null
 
 echo "Installing python dependencies"
-apt-get install -y build-essential python python-setuptools python3-pip python-dev > /dev/null
+sudo apt-get install -y build-essential python python-setuptools python3-pip python-dev > /dev/null
 
 echo "Installing Curl"
-apt-get install -y curl > /dev/null
+sudo apt-get install -y curl > /dev/null
 
 echo "Installing git. DO NOT COMMIT ON VM"
-apt-get install -y git > /dev/null
+sudo apt-get install -y git > /dev/null
 
 #install PostgreSQL
 echo "Installing and setting up PostgreSQL"
-apt-get install -y postgresql postgresql-contrib libpq-dev
+sudo apt-get install -y postgresql postgresql-contrib libpq-dev > /dev/null
 
-cat << EOF | su - postgres -c psql
+sudo cat << EOF | su - postgres -c psql
 -- Create the database user:
 CREATE USER admin WITH PASSWORD 'password';
 
@@ -26,32 +26,39 @@ EOF
 
 # install and setup a virtualenv
 echo "Installing Python virtualenv"
-pip3 install virtualenv > /dev/null
-echo "Setting up virutalenv at /vagrant"
-virtualenv /vagrant > /dev/null
-source /vagrant/bin/activate
+sudo pip3 install virtualenv
+echo "Setting up virutalenv at /vagrant/SigEpDev"
 cd /vagrant
+sudo virtualenv SigEpDev > /dev/null
+source /vagrant/SigEpDev/bin/activate > /dev/null
 
 # install pip packages
 echo "Installing Django 1.7 "
 pip install Django > /dev/null
 echo "Installing PostgreSQL connectors"
-pip install psycopg2
+pip install psycopg2 > /dev/null
 
 # install node and npm and update
 echo "Installing Node.js and NPM"
-curl -sL https://deb.nodesource.com/setup | sudo bash - > /dev/null
-apt-get install -y nodejs > /dev/null
-npm install npm -g > /dev/null
+sudo curl -sL https://deb.nodesource.com/setup | sudo bash - > /dev/null
+sudo apt-get install -y nodejs > /dev/null
+sudo npm install npm -g > /dev/null
+
+sudo apt-get clean > /dev/null
 
 # install bower
 echo "Installing bower"
-npm install -g bower > /dev/null
+sudo npm install -g bower > /dev/null
 
 # install SASS
-gem install sass
+echo "Installing SASS"
+sudo gem install sass > /dev/null
 
-#cleanup
-echo "Cleaning up"
-apt-get clean > /dev/null
+echo "Installing bower packages"
+bower install > /dev/null
 
+echo "Trying to start server"
+
+#cd source/
+# python manage.py runserver 0.0.0.0:8000 &
+# echo "Server running on 0.0.0.0:8000 (Access with localhost:4567)"
