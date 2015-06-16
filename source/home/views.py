c0b588921eeccdb5acf602826d5d1925fbf2afbe
Sheldon Burks <sheldon.burks@gmail.com>
2015-06-16 14:48:51 -0400

    Deleted the a unessecary html page that is not needed anymore.

--- source/home/views.py
+++ source/home/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 def index(request):
-	return render_to_response('index.html')
+    return render_to_response('index.html')
 
 def bms(request):
-	return render_to_response('bms.html')
+    return render_to_response('bms.html')
 
 def contact(request):
-	return render_to_response('contact.html')
+    return render_to_response('contact.html')
