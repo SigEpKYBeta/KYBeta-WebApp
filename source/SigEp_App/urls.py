af7067ea901d4cd96b2cc751cfe7cfad8e3d269d
Sheldon Burks <sheldon.burks@gmail.com>
2015-07-08 22:45:15 -0400

    Changed routes to not include the .html at the end of them.

--- source/SigEp_App/urls.py
+++ source/SigEp_App/urls.py
@@ -7,9 +7,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'SigEp_App.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'home.views.index'),
-    url(r'^index.html', 'home.views.index'),
-    url(r'^bms.html', 'home.views.bms'),
-    url(r'^contact.html', 'home.views.contact')
+    url(r'^index', 'home.views.index'),
+    url(r'^bms', 'home.views.bms'),
+    url(r'^contact', 'home.views.contact'),
 )
