e51a8532b1ee4b518c00ef781442325c5a85ea31
Sheldon Burks <sheldon.burks@gmail.com>
2015-11-26 12:15:32 -0600

    Added placeholder for member homepage

--- source/SigEp_App/urls.py
+++ source/SigEp_App/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^member/', include('member_home.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'home.views.index'),
     url(r'^index/', 'home.views.index'),
