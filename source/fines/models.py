967c4789df80907df04c91badb4f7431d3521658
Sheldon Burks <sheldon.burks@gmail.com>
2015-11-27 10:51:05 -0600

    Added a string method to the Fine model

--- source/fines/models.py
+++ source/fines/models.py
@@ -6,3 +6,8 @@ class Fine(models.Model):
     user = models.ForeignKey(User)
     amount = models.FloatField()
     reason = models.TextField()
+
+    def __str__(self):
+        return self.user.get_full_name() \
+                + ' - ' + str(self.amount) \
+                + ' - ' + self.reason
