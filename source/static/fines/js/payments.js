a16dbe333de2363fa70427f35c0d1b24b77c11dd
Sheldon Burks <sheldon.burks@gmail.com>
2015-12-20 12:45:10 -0500

    restructured static files to now be in a self contained directory for each app.

---
+++ source/static/fines/js/payments.js
@@ -0,0 +1,28 @@
+function totalSelectedFines() {
+    var total = 0;
+    $('#fineTable tr').find('input[type="checkbox"]:checked').each(function(index, row) {
+        console.log(row)
+    });
+}
+
+var handler = StripeCheckout.configure({
+    key: 'pk_test_5QJZJkQXysJq5Iz2Ewa0AjId',
+    locale: 'auto',
+    token: function(token) {
+        console.log(token);
+    }
+});
+
+$('#payFineButton').on('click', function(e) {
+    var fineTotal = totalSelectedFines();
+    handler.open({
+        email: 'sheldon.burks@gmail.com',
+        name: 'Paying 2 Fines ($20)',
+        amount: 2000
+    });
+    e.preventDefault();
+});
+
+$(window).on('popstate', function() {
+    handler.close()
+});
