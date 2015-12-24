3529be4b1de107366d6a72abd4df59afd07b4fa2
Sheldon Burks <sheldon.burks@gmail.com>
2015-12-23 21:08:08 -0600

    Added tests for Fines model

--- source/fines/tests.py
+++ source/fines/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import User
+
+from .models import Fine
+
+
+class FineModelTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user(
+                    email='test_email@gmail.com',
+                    first_name='Test',
+                    last_name='Case',
+                    password='123456')
+        Fine.objects.create(
+            user=user,
+            amount=20,
+            status='UNPAID',
+            reason='This is a test fine')
+
+    def test_fine_creation(self):
+        user = User.objects.get(email='test_email@gmail.com')
+        test_fine = Fine.objects.get(user=user)
+        
+        self.assertEqual(test_fine.amount, 20)
