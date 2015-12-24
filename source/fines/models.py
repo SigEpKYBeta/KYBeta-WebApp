49b2293c181b85771fa59688dd2133889e2f3cf8
Sheldon Burks <sheldon.burks@gmail.com>
2015-12-23 21:07:54 -0600

    Added a status to the fines and added a Fine Manager class

--- source/fines/models.py
+++ source/fines/models.py
@@ -2,10 +2,41 @@ from django.db import models
 from accounts.models import User
 
 
+class FinesManager(models.Manager):
+
+    def unpaid_fines(self):
+        return self.fines_by_status('UNPAID')
+
+    def paid_fines(self):
+        return self.fines_by_status('PAID')
+
+    def waived_fines(self):
+        return self.fines_by_status('WAIVED')
+
+    def fines_by_status(self, fine_status='UNPAID'):
+        fines = self.values('user__id',
+                            'user__first_name',
+                            'user__last_name') \
+                    .filter(status=fine_status) \
+                    .annotate(total=models.Sum('amount')) \
+                    .order_by('user__last_name')
+        return fines
+
+
 class Fine(models.Model):
+
+    STATUS_CHOICES = (
+            ('UNPAID', 'Unpaid'),
+            ('PAID', 'Paid'),
+            ('WAIVED', 'Waived'),
+        )
+
     user = models.ForeignKey(User)
     amount = models.FloatField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
     reason = models.TextField()
+
+    objects = FinesManager()
 
     def __str__(self):
         return self.user.get_full_name() \
