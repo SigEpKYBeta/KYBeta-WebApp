9e1dc9d5c855bf667b16e14bf0a5463ddfd935ff
Sheldon Burks <sheldon.burks@gmail.com>
2015-12-27 20:57:23 -0600

    Added initial support for paying Fines.

--- source/static/fines/js/userManager.js
+++ source/static/fines/js/userManager.js
@@ -1,28 +1,77 @@
-function totalSelectedFines() {
-    var selectedFines = [];
-    $('#fineTable tr').find('input[type="checkbox"]:checked').each(function(index, row) {
-        selectedFines.push(row.id);
-    });
-}
+$(function() {
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
 
-var handler = StripeCheckout.configure({
+  function csrfSafeMethod(method) {
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+      }
+    }
+  });
+
+  var handler = StripeCheckout.configure({
     key: 'pk_test_5QJZJkQXysJq5Iz2Ewa0AjId',
     locale: 'auto',
     token: function(token) {
-        console.log(token);
+      var selectedFines = getSelectedFines();
+      $.ajax({
+        type: 'POST',
+        url: '/fines/manage/paid/',
+        data: {fines: selectedFines}
+      });
+      location.reload();
     }
-});
+  });
 
-$('#payFineButton').on('click', function(e) {
-    var fineTotal = totalSelectedFines();
+  $(window).on('popstate', function() {
+    handler.close()
+  });
+
+  $('#payFineButton').on('click', function(e) {
+    var total = totalSelectedFines();
     handler.open({
-        email: 'sheldon.burks@gmail.com',
-        name: 'Paying 2 Fines ($20)',
-        amount: 2000
+      name: 'Fines Totaling $' + total,
+      amount: total * 100
     });
     e.preventDefault();
+  });
+
+  $('#fineTable tr').click(function(e) {
+    var total = totalSelectedFines();
+    $('#total').html('$' + total);
+  });
+
+  function getSelectedFines() {
+    var selectedFines = [];
+    $('#fineTable tr').find('input[type="checkbox"]:checked').each(function(index, row) {
+      selectedFines.push(row.id)
+    });
+    return selectedFines;
+  }
+
+  function totalSelectedFines() {
+    var fineTotal = 0;    
+    $('#fineTable tr').find('input[type="checkbox"]:checked').each(function(index, row) {
+      fineTotal += +$(row).data('amount');
+    });
+    return fineTotal;
+  }
 });
 
-$(window).on('popstate', function() {
-    handler.close()
-});
